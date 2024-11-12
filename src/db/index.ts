fa995c57bc27c942e473b5fff2941b60d4a670aa
Ajith <ajithkrm6@gmail.com>
2024-11-12 09:27:33 +0530

    updated db details

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,10 @@
 import mysql from "mysql2/promise";
 
-// const pool = mysql.createPool({
-//   host: process.env.MYSQL_HOST,
-//   user: process.env.MYSQL_DB_USER,
-//   password: process.env.MYSQL_DB_PASSWORD,
-//   database: process.env.MYSQL_DB,
-// });
-
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "8686668096@Aji",
-  database: "new_age_invitiation",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_DB_USER,
+  password: process.env.MYSQL_DB_PASSWORD,
+  database: process.env.MYSQL_DB,
 });
 
 const getConnection = async () => {
